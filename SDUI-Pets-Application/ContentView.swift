fc7783a9f42fa7456aac2bfff524e34bcc5baaec
Takasur Azeem <takasurazeem@gmail.com>
2022-12-02 20:30:18 +0500

    Initial Commit

---
+++ SDUI-Pets-Application/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SDUI-Pets-Application
+//
+//  Created by Takasur Azeem on 02/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SDUI-Pets-Application/SDUI_Pets_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDUI_Pets_ApplicationApp.swift
+//  SDUI-Pets-Application
+//
+//  Created by Takasur Azeem on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SDUI_Pets_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
